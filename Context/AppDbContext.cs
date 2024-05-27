e7dd63941b25bb242b67b380aa69eb06aaf22e68
Jorge <jorgetafurp@gmail.com>
2024-05-27 04:17:32 -0500

    Initial push

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Net_8_backend.Models;
+
+namespace Net_8_backend.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Net_8_backend.Models.Person> Person { get; set; } = default!;
+
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # net_8_crud_backend
+
+backend con .net
+crud basico (no autenticado)
+correr con visual studio 2022
